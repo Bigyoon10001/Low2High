7ccfc131bb40a808c94ce49d1aa3f5c943a6afb5
Bigyoon10001 <eodbs0117@gmail.com>
2022-04-21 21:48:01 +0900

    Study: core JS

---
+++ Study-CoreJS/01-16.js
@@ -0,0 +1,13 @@
+// 갹채의 깊은 복사를 수행하는 범용 함수
+
+var copyObjectDeep = function (target) {
+  var result = {};
+  if (typeof target === "object" && target !== null) {
+    for (var prop in target) {
+      result[prop] = copyObjectDeep(target[prop]);
+    }
+  } else {
+    result = target;
+  }
+  return result;
+};
